1dc9ff53ae850ded99b80792d5dd121142f09227
biatrix1995 <b.cordeiroaraujo@gmail.com>
2022-10-11 13:51:54 -0300

    Lista de exercícios Java

---
+++ JavaListaCincoExercicios/JavaLista5.java
@@ -0,0 +1,22 @@
+//Exercícios JAVA
+//
+//1) Escreva um programa que exiba as seguintes opções e realize os que se pede em cada uma delas: (switch-case)
+//
+//1 – Adição
+//2 – Subtração
+//3 – Multiplicação
+//4 – Divisão
+//
+//2) Faça um programa que exiba os números de 1 até 2000.
+//
+//3) Leia 2 vetores de inteiros V1 e V2 de N componentes cada (no máximo 50). Determine e imprima quantas vezes
+//que V1 e V2 possuem valores idênticos nas mesmas posições.
+//
+//4) Criar um programa que lê a idade de uma pessoa e diz se ela é maior de idade ou não.
+//
+//5) Criar programa java que lê N e depois N valores armazenado no array, em seguida retorna a soma total e a média dos valores.
+
+public class JavaLista5 {
+    public static void main(String[] args)
+        System.out.println();
+}
